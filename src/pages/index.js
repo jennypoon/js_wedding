0c4c453f1d377c71a8c3f3a31a1fef42c8ddfce5
Jenny Poon <jenny.poon@live.ca>
2019-02-12 17:03:21 -0800

    testing form in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import React from "react";
 // import Link from "gatsby-link";
 // import Helmet from "react-helmet";
 import RSVP from "../components/rsvp.js"
+import Test from "../components/test.js"
 
 export default class Index extends React.Component {
   render() {
     return (
       <div>
-        <RSVP />
+        <Test />
       </div>
     );
   }
